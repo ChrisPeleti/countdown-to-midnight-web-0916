42674e5dcf802b9a61d7b72c6461c8dcd5f84b41
Chris Peleti <chrisyred@gmail.com>
2016-09-24 19:31:52 +0000

    Happy New Year!

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  loop do
+    sleep(1)
+    puts "#{num} SECOND(S)!"
+    num-=1
+
+  if num==0
+    break
+  end
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  loop do
+    sleep(1)
+    puts "#{num} SECOND(S)!"
+    num-=1
+
+  if num==0
+    break
+  end
+  end
+  "HAPPY NEW YEAR!"
 end
